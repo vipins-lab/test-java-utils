a98f1f2eb584d4a99e51bd86527527800aa7a8d9
vipinkrajput <vipin.hbti.in@gmail.com>
2024-02-11 22:50:49 -0800

    initial code for test java project

---
+++ commons-utils/src/test/java/com/test/util/java/AppTest.java
@@ -0,0 +1,12 @@
+package com.test.util.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
