c404012e73ac019814c6f2fa483497eb24c5ada9
vipinkrajput <vipin.hbti.in@gmail.com>
2024-02-12 22:10:21 -0800

    Test greeting Util

---
+++ commons-utils/src/main/java/com/test/util/java/GreetingUtil.java
@@ -0,0 +1,10 @@
+package com.test.util.java;
+
+public class GreetingUtil {
+    public static final String FOO = "BAR";
+
+    public void printme() {
+        System.out.println("This is printme method");
+    }
+
+}
